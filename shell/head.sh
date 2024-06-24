909bc8ded6ec4bb9c1faab35dd845b39f7e9603d
jyi2ya <jyi2ya@outlook.com>
2024-06-24 06:54:42 +0000

    shell: remove all aliases before loading configuration

--- shell/head.sh
+++ shell/head.sh
@@ -5,8 +5,13 @@ case $- in
       *) return;;
 esac
 
+command_exists() {
+    { command -v "$1" || which "$1"; } >/dev/null
+}
+
 jf() {
     tmux attach-session -t X || tmux new-session -s X
 }
 
+unalias -a
 alias fj=jf
